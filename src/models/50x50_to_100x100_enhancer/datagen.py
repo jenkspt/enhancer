4b0695f50fa0ca8ccc95de5e3fe2b06401101d49
Penn Jenks <jenkspt@gmail.com>
2016-10-10 13:30:32 -0700

    enhancer working

---
+++ src/models/50x50_to_100x100_enhancer/datagen.py
@@ -0,0 +1,44 @@
+import os
+import re
+import numpy as np
+from PIL import Image, ImageFile
+import random
+from glob import glob
+
+
+def datagen(path, source_rescale=(50,50), target_size=(100, 100), batch_size=32):
+	files = glob(os.path.join(path, '*', '*.png'))
+	random.shuffle(files)
+	i = len(files)
+
+	while True:
+		i-=1
+		source = np.ndarray((batch_size, target_size[1], target_size[0], 1), dtype=np.float32)
+		target = np.ndarray((batch_size, target_size[1], target_size[0], 1), dtype=np.float32)
+
+		for j in range(batch_size):
+			ImageFile.LOAD_TRUNCATED_IMAGES = True
+			with Image.open(os.path.join(path, files[i])) as img:
+				img = img.convert('L').resize(target_size)
+				# %50 of the time flip images horizontally
+				if random.randint(0,1):
+					img = img.transpose(Image.FLIP_LEFT_RIGHT)
+
+				# Scale down the input and then scale it back up
+				source_img = img.resize(source_rescale).resize(target_size)
+				# PIL images are (width, height) and numpy arrays are (height, width)
+				source[j] = (np.asarray(source_img)/255).reshape(target_size[1], target_size[0], 1)
+				target[j] = (np.asarray(img)/255.).reshape(target_size[1], target_size[0], 1)
+
+		yield (source, target)
+
+
+
+		if i <= 0:
+			# After going through all the images reshuffle
+			i = len(files)
+			random.shuffle(files)
+
+if __name__ == '__main__':
+	gen = datagen('../../../data/bing/test')
+	x = next(gen)
---
+++ src/models/50x50_to_100x100_enhancer/model.py
@@ -0,0 +1,55 @@
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.models import Model, Sequential
+from keras.callbacks import TensorBoard, ModelCheckpoint
+
+import numpy as np
+
+from datagen import datagen
+
+# SETTINGS
+target_size = (100, 100)
+source_rescale = (50, 50)
+batch_size = 32
+nb_epoch = 50
+# training samples
+samples_per_epoch = 290496/32/4
+# testing samples
+nb_val_samples = 3020/32
+
+### THE MODEL ####
+
+input_img = Input(shape=(100, 100, 1))
+
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(input_img)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+
+
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='mse')
+
+
+### (end) THE MODEL ####
+
+### THE DATA ####
+
+train_generator = datagen('/home/ubuntu/enhancer/data/bing/train', source_rescale, target_size, batch_size)
+test_generator = datagen('/home/ubuntu/enhancer/data/bing/test', source_rescale, target_size, batch_size)
+
+
+autoencoder.fit_generator(train_generator,
+                validation_data=test_generator,
+                nb_epoch=nb_epoch,
+                samples_per_epoch=samples_per_epoch,
+                nb_val_samples=nb_val_samples,
+                nb_worker=1,
+                callbacks=[TensorBoard(log_dir='/tmp/enhancer', histogram_freq=0, write_graph=False),
+                ModelCheckpoint('saved_models/model', monitor='val_loss', mode='auto')]
+                )
