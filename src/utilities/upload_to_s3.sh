9f8f2854f0fb4e467a4b4b354ad3bd36220155c2
Penn Jenks <jenkspt@gmail.com>
2016-11-17 20:25:51 -0800

    removed unused files

--- src/utilities/upload_to_s3.sh
+++
@@ -1,5 +0,0 @@
-
-echo "Upload untouched compressed FERET dataset to s3"
-filename="test.txt"
-bucket="s3://enhancer/feret/"
-aws s3 cp $filename $bucket"original-"$filename --storage-class=STANDARD_IA --dryrun
